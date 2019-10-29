0f5919d2f73dfd05a9796f3eb18ba87087386808
3sdd <56486103+3sdd@users.noreply.github.com>
2019-10-29 14:00:06 +0900

    Update count_parameters.py

--- count_parameters.py
+++ count_parameters.py
@@ -10,16 +10,32 @@ def count_parameters(model):
 def count_trainable_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-
-
-if __name__=="__main__":
-    model_names=["alexnet",
+def get_model_names():
+        model_names=["alexnet",
         #vgg
         "vgg11","vgg11_bn","vgg13","vgg13_bn","vgg16","vgg16_bn","vgg19","vgg19_bn",
         #resnet
         "resnet18","resnet34","resnet50","resnet101","resnet152",
+        #squeeze net
+        "squeezenet1_0","squeezenet1_1",
+        #dense net
+        "densenet121","densenet169","densenet161","densenet201", # memory_efficient=False
+        #
+        "inception_v3","googlenet",
+        #shufflenet v2
+        "shufflenet_v2_x0_5","shufflenet_v2_x1_0","shufflenet_v2_x1_5","shufflenet_v2_x2_0",
+
+        "mobilenet_v2",
+        "resnext50_32x4d","resnext101_32x8d",
+        "wide_resnet50_2","wide_resnet101_2",
+        "mnasnet0_5","mnasnet0_75","mnasnet1_0","mnasnet1_3",
         ]
 
+
+        return model_names
+
+if __name__=="__main__":
+    model_names=get_model_names()
     li=[]
     for model_name in model_names:
 
